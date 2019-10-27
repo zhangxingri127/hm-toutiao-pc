0c2f571fc1ecad3b18e51585e9006b196e3973f6
zhangxingri <zhangdou127@outlook.com>
2019-10-27 10:55:37 +0800

    侧边栏完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Login from '@/views/login'
+import Home from '@/views/home'
 Vue.use(VueRouter)
 export default new VueRouter({
-  routes: [{ path: '/login', component: Login }]
+  routes: [{ path: '/login', component: Login },
+    { path: '/', component: Home }]
 })
---
+++ src/views/home/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <el-container class="container">
+    <el-aside width="200px">
+      <div class="logo"></div>
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        background-color="#002033"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+      >
+        <el-menu-item index="1">
+          <i class="el-icon-s-home"></i>
+          <span slot="title">首页</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-document"></i>
+          <span slot="title">内容管理</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-picture"></i>
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-s-promotion"></i>
+          <span slot="title">发布文章</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-chat-dot-round"></i>
+          <span slot="title">评论管理</span>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <i class="el-icon-present"></i>
+          <span slot="title">粉丝管理</span>
+        </el-menu-item>
+        <el-menu-item index="7">
+          <i class="el-icon-setting"></i>
+          <span slot="title">个人设置</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <span class="el-icon-s-fold icon"></span>
+        <span class="text">江苏传智播客科技教育有限公司</span>
+        <el-dropdown class="dropdown">
+          <span class="el-dropdown-link">
+            <img src="../../assets/avatar.jpg" alt />
+            下拉菜单
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item icon="el-icon-setting">个人设置</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-unlock">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  .el-aside {
+    background: #002033;
+  }
+  .el-header {
+    border-bottom: 1px solid #ddd;
+    line-height: 60px;
+    .text {
+      margin-left: 10px;
+    }
+    .dropdown {
+      float: right;
+      vertical-align: middle;
+
+      img {
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+        vertical-align: middle;
+      }
+    }
+  }
+  .logo {
+    width: 100%;
+    height: 60px;
+    background: #002244 url("../../assets/logo_admin.png") no-repeat center /
+      140px auto;
+  }
+  .el-menu {
+    border-right: none;
+  }
+}
+</style>
